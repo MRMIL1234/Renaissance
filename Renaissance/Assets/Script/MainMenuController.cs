7220c11ff569cb43ba88b0ee1f4347ea656022cb
mv12031990-create <mv12031990@gmail.com>
2026-06-09 16:03:37 +0300

    [WIP] feat: finalizing buttons prototype

--- Renaissance/Assets/Script/MainMenuController.cs
+++ Renaissance/Assets/Script/MainMenuController.cs
@@ -1,44 +1,50 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class MainMenuController : MonoBehaviour
 {
-    [Header("Panels")]
-    public GameObject creditsPanel;
-    public GameObject settingsPanel;
+    
+  
+    [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private GameObject settingsPanel;
 
     void Start()
     {
+        creditsPanel.SetActive(false);
         settingsPanel.SetActive(false);
     }
-
+ 
+    // --- Головне меню ---
+ 
     public void OnPlayClicked()
     {
         SceneManager.LoadScene("Game");
     }
-
+ 
     public void OnCreditsClicked()
     {
         creditsPanel.SetActive(true);
     }
-
+ 
     public void OnSettingsClicked()
     {
         settingsPanel.SetActive(true);
     }
-
-    public void OnCloseCreditsClicked()
-    {
-        creditsPanel.SetActive(false);
-    }
-
-    public void OnCloseSettingsClicked()
-    {
-        settingsPanel.SetActive(false);
-    }
-
+ 
     public void OnQuitClicked()
     {
         Application.Quit();
+    }
+ 
+    // --- Закрити панелі ---
+ 
+    public void OnCloseCreditsClicked()
+    {
+        creditsPanel.SetActive(false);
+        Debug.Log("Закриваємо панель з інформацією про розробників");
+    }
+ 
+    public void OnCloseSettingsClicked()
+    {
+        settingsPanel.SetActive(false);
     }
 }
