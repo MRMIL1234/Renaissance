adae10dd7261f79c61f0aa51b765c2ed1897ad78
MRMIL1234 <mihajlolavrov01@gmail.com>
2026-06-15 17:15:09 +0300

    feat: add health with slider and text

--- Renaissance/Assets/Scripts/BaseManager.cs
+++ Renaissance/Assets/Scripts/BaseManager.cs
@@ -1,28 +1,34 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BaseManager : MonoBehaviour
 {
+    [SerializeField] private UIManager uiManager;
+
     [SerializeField] private int _baseHealth = 100;
     [SerializeField] private int _baseCoins = 30;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public int BaseHealth
+    {
+        get { return _baseHealth; }
+        set
+        {
+            _baseHealth = value;
+            uiManager.UpdateHP(_baseHealth);
+        }
+    }
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        uiManager.UpdateHP(_baseHealth);
     }
     private void OnTriggerEnter2D (Collider2D other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
-            _baseHealth -= 10;
+            BaseHealth -= 10;
             Debug.Log("Hit");
-            Debug.Log("Base Health: " + _baseHealth);
-            if (_baseHealth <= 0)
+            Debug.Log("Base Health: " + BaseHealth);
+            if (BaseHealth <= 0)
             {
                 Debug.Log("Game Over!");
                 // Implement game over logic here
