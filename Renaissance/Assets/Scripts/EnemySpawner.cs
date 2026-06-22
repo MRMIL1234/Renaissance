d72f5c68e3b3356f24bbc3d0f530708835463e18
MRMIL1234 <mihajlolavrov01@gmail.com>
2026-06-22 20:35:31 +0300

    [WIP] feat: add variables for waves

--- Renaissance/Assets/Scripts/EnemySpawner.cs
+++ Renaissance/Assets/Scripts/EnemySpawner.cs
@@ -1,18 +1,23 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
-{
-    [SerializeField] private GameObject enemyPrefab; // Reference to the enemy prefab
-    [SerializeField] private float interval; // Time interval between spawns
-    [SerializeField] private Transform spawnPlace; // Position where enemies will be spawned
+{ 
+    [SerializeField] private float _interval; // Time interval between spawns
+
+    [SerializeField] private GameObject _enemyPrefab; // Reference to the enemy prefab
+    [SerializeField] private Transform _spawnPlace; // Position where enemies will be spawned
+
+    [SerializeField] private int _maxEnemiesInWave; // Maximum number of enemies to spawn in a wave
+    [SerializeField] private int _currentEnemiesInWave; // Current number of enemies spawned in the wave
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InvokeRepeating("SpawnEnemies", 0f, interval);
+        InvokeRepeating("SpawnEnemies", 0f, _interval);
     }
     public void SpawnEnemies()
     {
-        Instantiate(enemyPrefab, spawnPlace.position, Quaternion.identity);
+        Instantiate(_enemyPrefab, _spawnPlace.position, Quaternion.identity);
     }
     // Update is called once per frame
     void Update()
