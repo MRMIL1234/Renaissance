cd57b1b3e70dde50d6f0337024b1f37ed23ab043
MRMIL1234 <mihajlolavrov01@gmail.com>
2026-07-07 14:59:55 +0300

    fix: remove unused using and _baseCoins field in BaseManager

--- Renaissance/Assets/Scripts/BaseManager.cs
+++ Renaissance/Assets/Scripts/BaseManager.cs
@@ -1,13 +1,10 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class BaseManager : MonoBehaviour
 {
     [SerializeField] private UIManager uiManager;
 
     [SerializeField] private int _baseHealth = 100;
-    [SerializeField] private int _baseCoins = 30; // пока не используется, но пусть будет инкапсулирован
 
     public int BaseHealth
     {
