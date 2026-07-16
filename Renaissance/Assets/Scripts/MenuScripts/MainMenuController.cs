64ec2b2ca439d3d52756d3b24b2b64d7a01d7630
stonck <bogdans06045@gmail.com>
2026-07-16 15:23:42 +0300

    feat: road

--- Renaissance/Assets/Scripts/MenuScripts/MainMenuController.cs
+++ Renaissance/Assets/Scripts/MenuScripts/MainMenuController.cs
@@ -2,49 +2,66 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
-    
-  
+
+
     [SerializeField] private GameObject creditsPanel;
     [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject modePanel;
 
     void Start()
     {
         creditsPanel.SetActive(false);
         settingsPanel.SetActive(false);
+        modePanel.SetActive(false);
     }
- 
+
     // --- Головне меню ---
- 
+
     public void OnPlayClicked()
     {
-        SceneManager.LoadScene("Game");
+        modePanel.SetActive(true);
     }
- 
+
     public void OnCreditsClicked()
     {
         creditsPanel.SetActive(true);
     }
- 
+
     public void OnSettingsClicked()
     {
         settingsPanel.SetActive(true);
     }
- 
+
     public void OnQuitClicked()
     {
         Application.Quit();
     }
- 
+
     // --- Закрити панелі ---
- 
+
     public void OnCloseCreditsClicked()
     {
         creditsPanel.SetActive(false);
         Debug.Log("Закриваємо панель з інформацією про розробників");
     }
- 
+
     public void OnCloseSettingsClicked()
     {
         settingsPanel.SetActive(false);
+    }
+
+    public void OnCloseModeClicked()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    //--- Вибір режиму гри ---
+    public void OnEducationModeClicked()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void OnCampaignModeClicked()
+    {
+        SceneManager.LoadScene("LevelMap");
     }
 }
